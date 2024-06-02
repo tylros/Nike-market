d1425fe1afa4c55b14cabb27ab80ba0ea68184ad
ashiemdzkw <ashiemdzkw@gmail.com>
2024-06-03 00:11:14 +0200

    nike landing v0.1

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+import PopularProduts from './PopularProducts'
+import Footer from './Footer'
+
+export {
+ Hero,
+ Services,
+ SpecialOffer,
+ Subscribe,
+ SuperQuality,
+ PopularProduts,
+ Footer,
+ CustomerReviews
+}
